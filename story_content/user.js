9769c5371bb3238cc0a720740c2ae54fa7c8af51
MariaOctoD <maria@octod.ca>
2021-01-18 17:44:01 -0500

    testttt

---
+++ story_content/user.js
@@ -0,0 +1,761 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Bmme0O9G8o":
+        Script1();
+        break;
+      case "6019nR2C5ah":
+        Script2();
+        break;
+      case "63qAncRZMDR":
+        Script3();
+        break;
+      case "68f6fQZDPY6":
+        Script4();
+        break;
+      case "5o9ZAZrpfX8":
+        Script5();
+        break;
+      case "5meWuJ9A5v7":
+        Script6();
+        break;
+      case "6P5Su3SlU3A":
+        Script7();
+        break;
+      case "6EDYiZCaYVr":
+        Script8();
+        break;
+      case "5bf8u77G9Dy":
+        Script9();
+        break;
+      case "6SHd35KLezL":
+        Script10();
+        break;
+      case "6D565ZUYF9t":
+        Script11();
+        break;
+      case "6mAgd0bKe8z":
+        Script12();
+        break;
+      case "5tgJjyQ9oie":
+        Script13();
+        break;
+      case "5zJQmMz6f9n":
+        Script14();
+        break;
+      case "5yZ2McOgvtz":
+        Script15();
+        break;
+      case "5l5Oa2F2KwA":
+        Script16();
+        break;
+      case "6aktGti8oDV":
+        Script17();
+        break;
+      case "5kit6jfWiuJ":
+        Script18();
+        break;
+      case "6TDnnAfCdWQ":
+        Script19();
+        break;
+      case "6danhxDZrk2":
+        Script20();
+        break;
+      case "6TpH4ewDfh9":
+        Script21();
+        break;
+      case "6Blh61wIVy7":
+        Script22();
+        break;
+      case "6DL9O6VGBjJ":
+        Script23();
+        break;
+      case "5ni6gZdu38O":
+        Script24();
+        break;
+      case "5pgLiexuIAq":
+        Script25();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+
+$.ajax({
+
+url:
+"https://script.google.com/macros/s/AKfycbyHHoQPqxMzr8lIqxxWxTVY_Zj4oQefqOL0YdAXaxLJf2kpqB03mv8joA/exec",
+
+type: "POST",
+
+data: {"FirstName": player.GetVar("FirstName")
+, "LastName": player.GetVar("LastName")
+, "Email": player.GetVar("Email")
+
+},
+
+
+success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+
+$.ajax({
+
+url:
+"https://script.google.com/macros/s/AKfycbyHHoQPqxMzr8lIqxxWxTVY_Zj4oQefqOL0YdAXaxLJf2kpqB03mv8joA/exec",
+
+type: "POST",
+
+data: {"Email": player.GetVar("Email")
+, "Organization": player.GetVar("Organization")
+},
+
+
+success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+
+$.ajax({
+
+url:
+"https://script.google.com/macros/s/AKfycbyHHoQPqxMzr8lIqxxWxTVY_Zj4oQefqOL0YdAXaxLJf2kpqB03mv8joA/exec",
+
+type: "POST",
+
+data: {"Email": player.GetVar("Email")
+, "Location": player.GetVar("Location")
+},
+
+
+success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+
+$.ajax({
+
+url:
+"https://script.google.com/macros/s/AKfycbyHHoQPqxMzr8lIqxxWxTVY_Zj4oQefqOL0YdAXaxLJf2kpqB03mv8joA/exec",
+
+type: "POST",
+
+data: {"Email": player.GetVar("Email")
+, "OrganizationType": player.GetVar("OrganizationType")
+},
+
+
+success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+
+$.ajax({
+
+url:
+"https://script.google.com/macros/s/AKfycbyHHoQPqxMzr8lIqxxWxTVY_Zj4oQefqOL0YdAXaxLJf2kpqB03mv8joA/exec",
+
+type: "POST",
+
+data: {"Email": player.GetVar("Email")
+, "Affiliation": player.GetVar("Affiliation")
+},
+
+
+success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+
+$.ajax({
+
+url:
+"https://script.google.com/macros/s/AKfycbyHHoQPqxMzr8lIqxxWxTVY_Zj4oQefqOL0YdAXaxLJf2kpqB03mv8joA/exec",
+
+type: "POST",
+
+data: {"Email": player.GetVar("Email")
+, "OrganizationSize": player.GetVar("OrganizationSize")
+},
+
+
+success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+
+$.ajax({
+
+url:
+"https://script.google.com/macros/s/AKfycbyHHoQPqxMzr8lIqxxWxTVY_Zj4oQefqOL0YdAXaxLJf2kpqB03mv8joA/exec",
+
+type: "POST",
+
+data: {"Email": player.GetVar("Email")
+, "OrganizationSize": player.GetVar("OrganizationSize")
+},
+
+
+success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+
+}
+
+function Script8()
+{
+  var player = GetPlayer();
+
+$.ajax({
+
+url:
+"https://script.google.com/macros/s/AKfycbyHHoQPqxMzr8lIqxxWxTVY_Zj4oQefqOL0YdAXaxLJf2kpqB03mv8joA/exec",
+
+type: "POST",
+
+data: {"Email": player.GetVar("Email")
+, "OverseasExpenditures": player.GetVar("OverseasExpenditures")
+},
+
+
+success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+
+}
+
+function Script9()
+{
+  var player = GetPlayer();
+
+$.ajax({
+
+url:
+"https://script.google.com/macros/s/AKfycbyHHoQPqxMzr8lIqxxWxTVY_Zj4oQefqOL0YdAXaxLJf2kpqB03mv8joA/exec",
+
+type: "POST",
+
+data: {"Email": player.GetVar("Email")
+, "GACeligibility": player.GetVar("GACeligibility")
+},
+
+
+success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+
+}
+
+function Script10()
+{
+  var player = GetPlayer();
+
+$.ajax({
+
+url:
+"https://script.google.com/macros/s/AKfycbyHHoQPqxMzr8lIqxxWxTVY_Zj4oQefqOL0YdAXaxLJf2kpqB03mv8joA/exec",
+
+type: "POST",
+
+data: {"Email": player.GetVar("Email")
+, "Gender": player.GetVar("Gender")
+},
+
+
+success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+
+}
+
+function Script11()
+{
+  var player = GetPlayer();
+
+$.ajax({
+
+url:
+"https://script.google.com/macros/s/AKfycbyHHoQPqxMzr8lIqxxWxTVY_Zj4oQefqOL0YdAXaxLJf2kpqB03mv8joA/exec",
+
+type: "POST",
+
+data: {"Email": player.GetVar("Email")
+, "Age": player.GetVar("Age")
+},
+
+
+success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+
+}
+
+function Script12()
+{
+  var player = GetPlayer();
+
+$.ajax({
+
+url:
+"https://script.google.com/macros/s/AKfycbyHHoQPqxMzr8lIqxxWxTVY_Zj4oQefqOL0YdAXaxLJf2kpqB03mv8joA/exec",
+
+type: "POST",
+
+data: {"Email": player.GetVar("Email")
+, "English": player.GetVar("English")
+, "French": player.GetVar("French")
+},
+
+
+success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+
+}
+
+function Script13()
+{
+  var player = GetPlayer();
+
+$.ajax({
+
+url:
+"https://script.google.com/macros/s/AKfycbyHHoQPqxMzr8lIqxxWxTVY_Zj4oQefqOL0YdAXaxLJf2kpqB03mv8joA/exec",
+
+type: "POST",
+
+data: {"Email": player.GetVar("Email")
+, "Accessibility": player.GetVar("Accessibility")
+},
+
+
+success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+
+}
+
+function Script14()
+{
+  var player = GetPlayer();
+
+$.ajax({
+
+url:
+"https://script.google.com/macros/s/AKfycbyHHoQPqxMzr8lIqxxWxTVY_Zj4oQefqOL0YdAXaxLJf2kpqB03mv8joA/exec",
+
+type: "POST",
+
+data: {"Email": player.GetVar("Email")
+, "FirstTime": player.GetVar("FirstTime")
+},
+
+
+success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+
+}
+
+function Script15()
+{
+  var player = GetPlayer();
+
+$.ajax({
+
+url:
+"https://script.google.com/macros/s/AKfycbyHHoQPqxMzr8lIqxxWxTVY_Zj4oQefqOL0YdAXaxLJf2kpqB03mv8joA/exec",
+
+type: "POST",
+
+data: {"Email": player.GetVar("Email")
+, "Consent": player.GetVar("Consent")
+},
+
+
+success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+
+}
+
+function Script16()
+{
+  var player = GetPlayer();
+
+$.ajax({
+
+url:
+"https://script.google.com/macros/s/AKfycbyHHoQPqxMzr8lIqxxWxTVY_Zj4oQefqOL0YdAXaxLJf2kpqB03mv8joA/exec",
+
+type: "POST",
+
+data: {"Email": player.GetVar("Email")
+, "QuestTotal": player.GetVar("QuestTotal")
+},
+
+
+success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+
+}
+
+function Script17()
+{
+  var player = GetPlayer();
+
+$.ajax({
+
+url:
+"https://script.google.com/macros/s/AKfycbyHHoQPqxMzr8lIqxxWxTVY_Zj4oQefqOL0YdAXaxLJf2kpqB03mv8joA/exec",
+
+type: "POST",
+
+data: {"Email": player.GetVar("Email")
+, "QuestTotal": player.GetVar("QuestTotal")
+},
+
+
+success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+
+}
+
+function Script18()
+{
+  window.print();
+}
+
+function Script19()
+{
+  window.print();
+}
+
+function Script20()
+{
+  window.print();
+}
+
+function Script21()
+{
+  window.print();
+}
+
+function Script22()
+{
+  window.print();
+}
+
+function Script23()
+{
+  window.print();
+}
+
+function Script24()
+{
+  var player = GetPlayer();
+
+$.ajax({
+
+url:
+"https://script.google.com/macros/s/AKfycbyHHoQPqxMzr8lIqxxWxTVY_Zj4oQefqOL0YdAXaxLJf2kpqB03mv8joA/exec",
+
+type: "POST",
+
+data: {"Email": player.GetVar("Email")
+, "Assessment": player.GetVar("Assessment")
+},
+
+
+success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+
+}
+
+function Script25()
+{
+  var player = GetPlayer();
+
+$.ajax({
+
+url:
+"https://script.google.com/macros/s/AKfycbyHHoQPqxMzr8lIqxxWxTVY_Zj4oQefqOL0YdAXaxLJf2kpqB03mv8joA/exec",
+
+type: "POST",
+
+data: {"Email": player.GetVar("Email")
+, "Assessment": player.GetVar("Assessment")
+},
+
+
+success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+
+}
+
